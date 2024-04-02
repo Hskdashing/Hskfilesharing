cc9a97fe27c70e444b266578591fecfd178a6a22
Hsk <149724465+Hskdashing@users.noreply.github.com>
2024-04-02 08:45:42 +0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,53 +1,57 @@
+# Credits: @hskdashing
+# FROM File-Sharing-Man <https://github.com/Hskdashing/Brainpepsfile/>
+# t.me/hskdashing & t.me/hskdashing
+
 from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Commands for Bot Users
- ├ /start - Start the Bot
- ├ /about - About this Bot
- ├ /help - Help with Bot commands
- ├ /ping - Check if bot is alive
- └ /uptime - Check bot's status 
+<b> ❏ How to use this boat
+ ├ /start - start Bot
+ ├ /about - about this bot
+ ├ /help - help this Bot 
+ ├ /ping - to check live bots
+ └ /uptime - to see status 
  
- ❏ Commands for Bot Admins
- ├ /logs - View bot logs
- ├ /setvar - Set variables using bot commands
- ├ /delvar - Delete variables using bot commands
- ├ /getvar - View specific variable using bot commands
- ├ /users - View bot users statistics
- ├ /batch - Create links for multiple files
- ├ /speedtest - Test bot server's speed
- └ /broadcast - Send broadcast message to bot users
+ ❏ Commands for BOT Admin
+ ├ /logs - to view bot logs
+ ├ /setvar - to set var with command dibotted
+ ├ /delvar - delvar-to delete a var with a bot command
+ ├ /getvar - to view one of the VARs with the command dibotted
+ ├ /users - to view bot User Statistics
+ ├ /batch - batch-to link more than one file
+ ├ /speedtest - to test the speed of the bot server
+ └ /broadcast - to send a broadcast message to the bot user
 
-👨‍💻 Developed by </b><a href='https://t.me/hskdashing/101'>@hskdashing</a>
+👨‍💻 Develoved by </b><a href='https://t.me/@Zblivebot'>@Zblivebot</a>
 """
 
     close = [
-        [InlineKeyboardButton("Close", callback_data="close")]
+        [InlineKeyboardButton("close", callback_data="close")]
     ]
 
     mbuttons = [
         [
-            InlineKeyboardButton("Help & Commands", callback_data="help"),
-            InlineKeyboardButton("Close", callback_data="close")
+            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            InlineKeyboardButton("close", callback_data="close")
         ],
     ]
 
     buttons = [
         [
-            InlineKeyboardButton("About Me", callback_data="about"),
-            InlineKeyboardButton("Close", callback_data="close")
+            InlineKeyboardButton("about me", callback_data="about"),
+            InlineKeyboardButton("close", callback_data="close")
         ],
     ]
 
     ABOUT = """
-<b>About this Bot:
+<b>Tentang Bot ini:
 
-@{} is a Telegram Bot for storing Posts or Files that can be Accessed through a Special Link.
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
 
  • Creator: @{}
  • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
- • Source Code: <a href='https://'>hsk v4</a>
-"""
-👨‍💻 Develoved by </b><a href='https://t.me/hskdashing/101'>@hskdashing</a>
+ • Source Code: <a href='https://t.me/@Zblivebot</a>
+
+👨‍💻 Develoved by </b><a href='https://t.me/@Zblivebot'>@Zblivebot</a>
 """
