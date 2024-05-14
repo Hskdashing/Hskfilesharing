32d6f034b8fb73d8b6d0dcdb53122193a8913462
Hsk <149724465+Hskdashing@users.noreply.github.com>
2024-05-14 12:16:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ load_dotenv("config.env")
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 # API ID Anda dari my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "25582726"))
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 # API Hash Anda dari my.telegram.org
-API_HASH = os.environ.get("API_HASH", "558df3cdc4820fd2de0950656e8112f3")
+API_HASH = os.environ.get("API_HASH", "")
 
 # ID Channel Database
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001956409311"))
